fcf9c796c287c91a434092785ca6310285a5a1fd
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-24 23:15:06 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -46,7 +46,7 @@ class ArtistsController < ApplicationController
 
   private
 
-  def artist_params
-    params.require(:artist).permit(:name)
+  def artist_params(args*)
+    params.require(:artist).permit(*args)
   end
 end
